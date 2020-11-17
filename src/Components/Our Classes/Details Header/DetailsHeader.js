39d5164af94980bef68c9f30d50e809a335906e2
Shaker <sakerahmmed978@gmail.com>
2020-11-17 13:54:53 +0600

    add something

--- src/Components/Our Classes/Details Header/DetailsHeader.js
+++ src/Components/Our Classes/Details Header/DetailsHeader.js
@@ -1,10 +1,12 @@
 import React from 'react';
-
 import Navbar from '../../Shared/Navbar/Navbar';
-import './DetailsHeader.css';
+
 
 const DetailsHeader = () => {
-
+    return (
+        <div className='class-header-style'>
+            <Navbar></Navbar>
+            <h1 className='text-center class-heading'>ADVANCE GYM</h1>
         </div>
     );
 };
